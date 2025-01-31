49d39cbbb81d95b1526d46fdba5a111ab4fa103e
Usman Pervez <usmanpervez5@gmail.com>
2025-01-31 21:26:32 +0500

    Implement user signup functionality and user model

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,9 +1,54 @@
-
+import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
 
 export const signup = async (req,res) => {
-    res.json({
-        data: "You hit the signup endpoint"
-    })
+    try {
+        const {username, fullName, password, email} = req.body;
+
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        if(!emailRegex.test(email)){
+            return res
+            .status(400)
+            .json({
+                error: "Invalid email format"
+            })
+        }
+
+        const existingUser = await User.findOne({ username })
+        if(existingUser){
+            return res
+            .status(400)
+            .json({
+                error: "Username already taken"
+            })
+        }
+
+        const existingEmail = await User.findOne({ email })
+        if(existingEmail){
+            return res
+            .status(400)
+            .json({
+                error: "Email already taken"
+            })
+        }
+
+
+        const salt = await bcrypt.genSalt(10)
+
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const newUser = new User({
+            fullName,
+            username,
+            email,
+            password: hashedPassword
+        })
+
+        
+
+    } catch (error) {
+        
+    }
 }
 
 export const login = async (req,res) => {
--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,15 @@ import authRoutes from "./routes/auth.route.js"
 import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongoDB.js"
 
-const app = express();
+dotenv.config()
 
+const app = express();
 const PORT = process.env.PORT || 5000
+
+app.use(express.json()) // to parse req.body
 
 app.use("/api/auth", authRoutes)
 
-dotenv.config()
-console.log(process.env.MONGO_URI);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
