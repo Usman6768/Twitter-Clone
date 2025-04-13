e75f81fd9042ac7a5a2f6d44b5a8b3be3384df30
Usman Pervez <usmanpervez5@gmail.com>
2025-04-13 12:12:25 +0500

    Enhance RightPanel to fetch suggested users dynamically using react-query

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 5000
 
-app.use(express.json()) // to parse req.body
+app.use(express.json({limit: "2mb"})) // to parse req.body
 app.use(express.urlencoded({extended: true})) // to parse req.body
 app.use(cookieParser())
 
